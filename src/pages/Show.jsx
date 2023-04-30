ea1b6901c9f100cf7af18206764fc6fd1788dac6
Shrey Agarwal <shreyagarwal1528@gmail.com>
2023-04-30 22:12:08 +0530

    Installed react query

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,32 +1,16 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
-
-const useShowById = showId => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (error) {
-        setShowError(error);
-      }
-    }
-    fetchData();
-  }, [showId]);
-
-  return showData, showError;
-};
+import { useQuery } from '@tanstack/react-query';
 
 const Show = () => {
   const { showId } = useParams();
-  const { showData, showError } = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
-    return <div>We have an errroe:{showError.message}</div>;
+    return <div>We have an errror:{showError.message}</div>;
   }
   if (showData) {
     return <div>Got show data:{showData.name}</div>;
